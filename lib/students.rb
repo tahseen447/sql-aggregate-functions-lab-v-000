4842fef9e06e3c32d2e5760490b39439640b5497
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-06 22:48:07 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "select CSUM(tardies) from students"
+  "select SUM(tardies) from students"
 end
 
 def average_gpa_for_9th_grade
